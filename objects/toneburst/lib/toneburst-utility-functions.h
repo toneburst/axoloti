872dd43a4e53c43880d5db48df462af20287b141
alx <the_voder@yahoo.co.uk>
2016-07-14 09:23:30 +0100

    commit from axoloti UI

--- objects/toneburst/lib/toneburst-utility-functions.h
+++ objects/toneburst/lib/toneburst-utility-functions.h
@@ -80,4 +80,14 @@
 		return (int)aif;
 	}
 
+    // Check size of binary file
+    // http://stackoverflow.com/questions/238603/how-can-i-get-a-files-size-in-c
+    int fsize(FILE *fp){
+        int prev=ftell(fp);
+        fseek(fp, 0L, SEEK_END);
+        int sz=ftell(fp);
+        fseek(fp,prev,SEEK_SET); //go back to where we were
+        return sz;
+    }
+
 #endif
